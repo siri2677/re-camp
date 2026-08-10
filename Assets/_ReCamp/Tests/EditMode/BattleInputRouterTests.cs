660ab8f0a7b6737d36ae7e64ae439d8f46522444
lime <siri9108@naver.com>
2026-08-10 11:22:19 +0900

    test: cover landscape settings and multitouch ownership

--- Assets/_ReCamp/Tests/EditMode/BattleInputRouterTests.cs
+++ Assets/_ReCamp/Tests/EditMode/BattleInputRouterTests.cs
@@ -1,6 +1,9 @@
 using NUnit.Framework;
 using ReCamp.Input;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace ReCamp.Tests.EditMode
 {
@@ -80,6 +83,56 @@ namespace ReCamp.Tests.EditMode
             Assert.That(router.MoveInput, Is.EqualTo(Vector2.zero));
             Assert.That(router.IsExtractionHeld, Is.False);
             Assert.That(signatureReleased, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TouchHoldButton_IgnoresReleaseFromDifferentPointer()
+        {
+            var eventSystemObject = new GameObject("TouchHoldButtonEventSystem", typeof(EventSystem));
+            var buttonObject = new GameObject("TouchHoldButtonTest", typeof(RectTransform), typeof(Button));
+            try
+            {
+                var eventSystem = eventSystemObject.GetComponent<EventSystem>();
+                var holdButton = buttonObject.AddComponent<TouchHoldButton>();
+                holdButton.Configure(TouchAction.Signature);
+
+                var pressed = 0;
+                var released = 0;
+                router.SignaturePressed += () => pressed++;
+                router.SignatureReleased += () => released++;
+
+                var ownerPointer = new PointerEventData(eventSystem) { pointerId = 101 };
+                var otherPointer = new PointerEventData(eventSystem) { pointerId = 202 };
+
+                holdButton.OnPointerDown(ownerPointer);
+                holdButton.OnPointerDown(otherPointer);
+                holdButton.OnPointerUp(otherPointer);
+
+                Assert.That(holdButton.IsHeld, Is.True);
+                Assert.That(holdButton.ActivePointerId, Is.EqualTo(101));
+                Assert.That(pressed, Is.EqualTo(1));
+                Assert.That(released, Is.EqualTo(0));
+
+                holdButton.OnPointerUp(ownerPointer);
+
+                Assert.That(holdButton.IsHeld, Is.False);
+                Assert.That(released, Is.EqualTo(1));
+            }
+            finally
+            {
+                Object.DestroyImmediate(buttonObject);
+                Object.DestroyImmediate(eventSystemObject);
+            }
+        }
+
+        [Test]
+        public void AndroidPlayerSettings_AutorotationIsLandscapeOnly()
+        {
+            Assert.That(PlayerSettings.defaultInterfaceOrientation, Is.EqualTo(UIOrientation.AutoRotation));
+            Assert.That(PlayerSettings.allowedAutorotateToPortrait, Is.False);
+            Assert.That(PlayerSettings.allowedAutorotateToPortraitUpsideDown, Is.False);
+            Assert.That(PlayerSettings.allowedAutorotateToLandscapeLeft, Is.True);
+            Assert.That(PlayerSettings.allowedAutorotateToLandscapeRight, Is.True);
         }
     }
 }
